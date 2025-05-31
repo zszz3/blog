8925490528e31797c64f24b1f0a2179e5c38bcf7
wojiecihuo <91608029+zszz3@users.noreply.github.com>
2025-05-31 10:29:19 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://zszz3.github.io/",
-	base: "/base",
+	base: "/blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
