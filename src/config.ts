8c7988de2f2beb674e4aa0a525c62c279f5dafa5
zszz3 <wojiecihuo@gmail.com>
2025-08-02 22:07:37 +0800

    init

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
+	title: "wojiecihuo",
 	subtitle: "Demo Site",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th', 'vi'
 	themeColor: {
