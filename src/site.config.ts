fdf93957d7355f80ee26b73e8fa3ad9f90e8ed29
JYXC-DZ-0101263 <jyxc-dz-0101263@JYXC-DZ-0101263deMacBook-Pro.local>
2026-09-12 19:43:32 +0800

    fix: replace docs navigation with article archives

--- src/site.config.ts
+++ src/site.config.ts
@@ -49,7 +49,7 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: '博客', link: '/blog' },
-      { title: '文档', link: '/docs' },
+      { title: '归档', link: '/archives' },
       { title: '项目', link: '/projects' },
       { title: '友链', link: '/links' },
       { title: '关于', link: '/about' }
